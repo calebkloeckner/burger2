4b8faec62a4c226d8f9fe8e284cceadd19ad249c
calebkloeckner <calebkloeckner@gmail.com>
2018-02-26 18:38:30 -0700

    kinda working

---
+++ config/orm.js
@@ -0,0 +1,26 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  all: function(tableInput, cb){
+    connection.query("SELECT * FROM " +tableInput+";", function(error, result) {
+      if(error) throw error;
+      cb(result)
+    })
+  },
+
+  update: function(tableInput, condition, cb) {
+    connection.query("UPDATE "+tableInput+" SET devoured=true WHERE id="+condition+";", function(error, result){
+      if(error)throw error;
+      cb(result);
+    })
+  },
+
+  create: function(tableInput, val, cb) {
+    connection.query("INSERT INTO "+tableInput+" (burger_name)VALUES ('"+val+"');", function(error, result){
+      if(error) throw error;
+      cd(results);
+    })
+  }
+}
+
+module.exports = orm;
